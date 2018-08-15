461407e560b90607d70cc1c455445db4eb5fe1cd
Arvin Zojaji <arvin.zojaji@gmail.com>
2018-08-15 18:30:09 -0400

    creating javascript folder and app.js, file

---
+++ assets/javascript/app.js
@@ -0,0 +1,6 @@
+// get top 5 currencies
+top5QueryURL = "https://min-api.cryptocompare.com/data/top/totalvol?limit=5&tsym=USD"
+// return the top 5 symbols from the above ajax call, insert them into an array and call the below function to create a new query
+function streamQueryParams(coins) {
+    
+}
