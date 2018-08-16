f053551f0a389c3d6aeabb9d9f29ce87bd16411c
Arvin Zojaji <arvin.zojaji@gmail.com>
2018-08-16 09:02:27 -0400

    further experimentation with AJAX calls, changed jQuery source from slim build as it does not include AJAX

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,24 @@
-// get top 5 currencies for test build, will increase to higher amounts later
-var top5QueryURL = "https://min-api.cryptocompare.com/data/top/totalvol?limit=5&tsym=USD"
+$( document ).ready(function () {// get top 5 currencies for test build, will increase to higher amounts later
+    var top5QueryURL = "https://min-api.cryptocompare.com/data/top/totalvol?limit=5&tsym=USD&extraParams=CryptoPlay"
 
-// return the top 5 coin symbols from the above ajax call, insert them into an array and call the below function to create a new query
-// Let's store those symbols in the variable 'coins'
+    // return the top 5 coin symbols from the above ajax call, insert them into an array and call the below function to create a new query
+    // Let's store those symbols in the variable 'coins'
 
-$.ajax({
-    url: top5QueryURL,
-    method: "GET"
-}).then(function (topCoinsResponse) {
-    var params = "";
-    for (var i = 0; i <= 4; i++) {
-        params += topCoinsResponse.Data[i].CoinInfo.Internal + ",";
-    };
-    var streamQueryURL = "https://min-api.cryptocompare.com/data/subsWatchlist?fsyms=" + params + "&tsym=USD"
     $.ajax({
-        url: streamQueryURL,
+        url: top5QueryURL,
         method: "GET"
-    }).then(function (streamResponse) {
-        console.log(streamResponse);
+    }).then(function (topCoinsResponse) {
+        console.log(topCoinsResponse);
+        var params = "";
+        for (var i = 0; i <= 4; i++) {
+            params += topCoinsResponse.Data[i].CoinInfo.Internal + ",";
+        };
+        var streamQueryURL = "https://min-api.cryptocompare.com/data/subsWatchlist?fsyms=" + params + "&tsym=USD&extraParams=CryptoPlay"
+        $.ajax({
+            url: streamQueryURL,
+            method: "GET"
+        }).then(function (streamResponse) {
+            console.log(streamResponse);
+        });
     });
 });
-
