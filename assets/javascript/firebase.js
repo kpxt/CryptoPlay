67a5d5dce19aced298176852f41f491317e268e6
kalistan <kaliesha.pickering@gmail.com>
2018-08-16 21:13:29 -0400

    debugging

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -1,5 +1,5 @@
 // Initialize Firebase
- var config = {
+var config = {
   apiKey: "AIzaSyAlIEi8_mjg-nMMp7wD1cFfqq22H8Oz75I",
   authDomain: "project-x-b0368.firebaseapp.com",
   databaseURL: "https://project-x-b0368.firebaseio.com",
@@ -11,14 +11,24 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
+var username = "";
+var email = "";
+var pwd = "";
+var cfmPwd = "";
+var username2 = "";
+var loginPwd = "";
+
+
 //register user
-$("#register").on("click", function (event) {
+$("#btnSubmit").on("click", function (event) {
+ 
   event.preventDefault();
 
-  var username = $("#usernameInput").val().trim();
-  var email = $("#EmailInput1").val().trim();
-  var pwd = $("#InputPwd1").val().trim();
-  var cfmPwd = $("#confirmPassword").val().trim();
+  username = $("#usernameInput").val().trim();
+  email = $("#emailInput1").val().trim();
+  pwd = $("#inputPwd1").val().trim();
+  cfmPwd = $("#confirmPassword").val().trim();
+
 
   var newUser = {
     username: username,
@@ -27,28 +37,40 @@ $("#register").on("click", function (event) {
     confirm: cfmPwd,
     dateAdded: firebase.database.ServerValue.TIMESTAMP
   };
+  //push to firebase
   database.ref().push(newUser);
+
+  //log new user
+  console.log(newUser.username);
+  console.log(newUser.email);
+  console.log(newUser.password);
+  console.log(newUser.confirm);
 
   //clear fields
   clear();
 
-}, function (errorObject) {
-  console.log(errorObject)
+
 });
 
 //login user
 $("#logIn").on("click", function (event) {
   event.preventDefault();
 
-  var username2 = $("#usernameInput").val().trim();
-  var loginPwd = $("#passwordInput2").val().trim();
+  username2 = $("#usernameInput").val().trim();
+  loginPwd = $("#passwordInput2").val().trim();
+  console.log(username2);
+  console.log(loginPwd);
 
+
+  clear();
 });
 
 //clear all user fields
 function clear() {
   $("#usernameInput").val("");
-  $("#EmailInput1").val("");
-  $("#InputPwd1").val("");
+  $("#emailInput1").val("");
+  $("#inputPwd1").val("");
   $("#confirmPassword").val("");
+  $("#usernameInput").val("");
+  $("#passwordInput2").val("");
 };
